ed42f287bc309f618f60d33f1d87fd8b0c991e1a
Ye Yangchen <eikasia30@gmail.com>
2023-12-28 19:49:30 -0500

    lab1

--- lab01/ex10_ll_cycle.c
+++ lab01/ex10_ll_cycle.c
@@ -2,5 +2,22 @@
 #include "ex10_ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+  node *fast_ptr = head;
+  node *slow_ptr = head;
+  
+  for (;;) {
+    if (fast_ptr == NULL) {
+      return 0;
+    }
+    fast_ptr = fast_ptr->next;
+    if (fast_ptr == NULL) {
+      return 0;
+    }
+    fast_ptr = fast_ptr->next;
+    slow_ptr = slow_ptr->next;
+
+    if (fast_ptr == slow_ptr) {
+      return 1;
+    }
+  }
 }
